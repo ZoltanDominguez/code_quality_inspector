0b297b47ddfeaed38558f1d7f5ce2fadcdb3a856
Zoltan Dominguez <zoltan.dominguez@edu.bme.hu>
2023-05-07 00:41:56 +0200

    add pylint

--- tests/api_functional_tests/test_coverage.py
+++ tests/api_functional_tests/test_coverage.py
@@ -1,11 +1,17 @@
 from pathlib import Path
 
+from fastapi import FastAPI
+from starlette.testclient import TestClient
+
+from code_quality_inspector.app.coverage_router import coverage_router
+from code_quality_inspector.app.endpoints import COVERAGE_ENDPOINT
+
 APP_ROOT_DIR = Path(__file__).parent.parent.parent.resolve()
 
 
 def test_ep_with_test_client():
     """Test stub using test client"""
-    '''app = FastAPI(debug=False)
+    app = FastAPI(debug=False)
     app.include_router(coverage_router)
     fast_api_client = TestClient(app)
 
@@ -14,4 +20,4 @@ def test_ep_with_test_client():
     expected_code = 200
     assert (
         response.status_code == expected_code
-    ), f"Endpoint:{COVERAGE_ENDPOINT} not returned the {expected_code=}"'''
+    ), f"Endpoint:{COVERAGE_ENDPOINT} not returned the {expected_code=}"
--- tests/unit_tests/test_comment_pr.py
+++ tests/unit_tests/test_comment_pr.py
@@ -5,13 +5,17 @@ from code_quality_inspector.github_connector.comment_to_pr import get_comments_t
 
 
 def test_comment_deletion_based_on_datetime():
-    d1 = datetime(2010, 1, 1, 18, 00)
-    d2 = datetime(2010, 1, 1, 19, 00)
-    d3 = datetime(2010, 1, 1, 18, 30)
-    comments = [Mock(created_at=d1), Mock(created_at=d2), Mock(created_at=d3)]
+    date_1 = datetime(2010, 1, 1, 18, 00)
+    date_2 = datetime(2010, 1, 1, 19, 00)
+    date_3 = datetime(2010, 1, 1, 18, 30)
+    comments = [
+        Mock(created_at=date_1),
+        Mock(created_at=date_2),
+        Mock(created_at=date_3),
+    ]
 
     comments_to_delete = get_comments_to_delete(comments)
     for comment in comments_to_delete:
         print(comment.created_at)
-        assert comment.created_at != d2
+        assert comment.created_at != date_2
     assert len(comments_to_delete) == 2
