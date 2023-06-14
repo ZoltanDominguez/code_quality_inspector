326c8e62c0ca4270c0cf7326f163473973534703
Zoltan Dominguez <zoltan.dominguez@edu.bme.hu>
2023-06-14 23:54:00 +0200

    start webhook impl

--- cqi/app/webhook_router.py
+++ cqi/app/webhook_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 
 from cqi.app.endpoints import GITHUB_WEBHOOK_ENDPOINT
 from cqi.github_connector.comment_to_pr import comment_to_pr
@@ -7,7 +7,8 @@ github_webhook_router = APIRouter()
 
 
 @github_webhook_router.post(
-    path=GITHUB_WEBHOOK_ENDPOINT,
+    path=GITHUB_WEBHOOK_ENDPOINT + "/{project_name}",
 )
-def reports() -> None:
+def webhook() -> Response:
     comment_to_pr(repo_full_name="demo", pr_id=1, content="description")
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
