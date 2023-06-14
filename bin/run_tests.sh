5f446cb92802e5385f31ef1a3cb0acb60632461f
Zoltan Dominguez <zoltan.dominguez@edu.bme.hu>
2023-06-14 22:47:00 +0200

    use abbreviation

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -14,43 +14,42 @@ pytest ./tests/ \
        --html "./reports/html/test_results.html" \
        --cov-report xml:./reports/xml/test_coverage.xml \
        --cov-report html:./reports/html/test_coverage \
-       --cov=code_quality_inspector --cov-branch
+       --cov=cqi --cov-branch
 
 TESTS_EXIT_CODE=$?
 echo "Exit code for tests: " $TESTS_EXIT_CODE
 
 if [[ "$TESTS_EXIT_CODE" == 0 ]] 
 then
-  coverage report --fail-under=$MIN_TEST_COVERAGE
-  COVERAGE_EXIT_CODE=$?
-  echo "Coverage exit code for tests: " $COVERAGE_EXIT_CODE
+  coverage report --fail-under=$MIN_TEST_COVERAGE;            COVERAGE_EXIT_CODE=$?
+  echo "Coverage exit code: " $COVERAGE_EXIT_CODE
 fi
 
 print_line
 echo "Running pylint:"
-pylint code_quality_inspector/;          PYLINT_EXIT_CODE=$?
+pylint cqi/;                                                  PYLINT_EXIT_CODE=$?
 
 echo "Running ruff:"
-ruff check code_quality_inspector/;      RUFF_EXIT_CODE=$?
+ruff check cqi/;                                              RUFF_EXIT_CODE=$?
 if [ $RUFF_EXIT_CODE -eq 0 ]; then echo "Ruff passed!"; fi
 
 print_line && echo "Running black:"
-black . --check;                         BLACK_EXIT_CODE=$?
+black . --check;                                              BLACK_EXIT_CODE=$?
 
 print_line && echo "Running semgrep:"
-semgrep scan --config auto;              SEMGREP_EXIT_CODE=$?
+semgrep scan --config auto;                                   SEMGREP_EXIT_CODE=$?
 
 print_line && echo "Running mypy:"
-mypy code_quality_inspector/;            MYPY_EXIT_CODE=$?
+mypy cqi/;                                                    MYPY_EXIT_CODE=$?
 
 print_line && echo "Summary:"
-echo "Tests exit code: " $TESTS_EXIT_CODE
+echo "Tests exit code: "    $TESTS_EXIT_CODE
 echo "Coverage exit code: " $COVERAGE_EXIT_CODE
-echo "Pylint exit code: " $PYLINT_EXIT_CODE
-echo "Ruff exit code: " $RUFF_EXIT_CODE
-echo "Black exit code: " $BLACK_EXIT_CODE
-echo "Semgrep exit code: " $SEMGREP_EXIT_CODE
-echo "Mypy exit code: " $MYPY_EXIT_CODE
+echo "Pylint exit code: "   $PYLINT_EXIT_CODE
+echo "Ruff exit code: "     $RUFF_EXIT_CODE
+echo "Black exit code: "    $BLACK_EXIT_CODE
+echo "Semgrep exit code: "  $SEMGREP_EXIT_CODE
+echo "Mypy exit code: "     $MYPY_EXIT_CODE
 
 
 if  [ $TESTS_EXIT_CODE -ne 0 ] || \
